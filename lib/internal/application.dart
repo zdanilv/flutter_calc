1de12ba189f40aa5cc01ae1ddd9dee393fceb53e
zdanilv <zdanilv83@yandex.ru>
2023-08-17 12:24:06 +0300

    base

---
+++ lib/internal/application.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calc/presentation/home.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
